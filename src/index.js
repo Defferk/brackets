d92a43d38b57baa372af535001396cd0145ed480
Defferk <a.dobryetsov@gmail.com>
2022-08-13 00:45:26 +0300

    done bracets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  var loop=0;
+  var strLeng = str.length;
+  bracketsConfig.forEach((rule,ID) => {
+      bracketsConfig[ID] = rule[0]+rule[1]
+  });
+  while(str.length>0){
+      for(let i=0;i<bracketsConfig.length;i++){
+          str = str.replace(`${bracketsConfig[i]}`,'')
+          if(str.length==strLeng){
+              loop++;
+              continue;
+          }
+          strLeng=str.length;
+          loop=0;
+      }
+      if(loop>bracketsConfig.length){return false;}
+  }
+  return  true;
 }
